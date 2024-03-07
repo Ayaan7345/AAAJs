9aec7733320ed093cc72437199d0de9989c5c2c5
Ayaan7345 <ayaan7345@gmail.com>
2024-03-07 10:16:40 -0600

    inital commit

--- uinputmenu.java
+++ uinputmenu.java
@@ -3,7 +3,7 @@ public class uinputmenu {
     // Declare class members
     public void menuUsher() {
     System.out.println("This is a test to see if i have coded OOP correctly") ;
-   
+
 
     } //checking if it works
 }    
